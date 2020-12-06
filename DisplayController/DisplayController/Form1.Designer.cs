82909155208d4e74d1f515157b6965ac18e641c0
Sam Faull <samz5991@hotmail.co.uk>
2020-12-06 15:51:38 +0000

    Real basic winforms app to connect to the device and send a 60x60 partial frame to the device

---
+++ DisplayController/DisplayController/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DisplayController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnIDN = new System.Windows.Forms.Button();
+            this.btnGetBuff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(248, 40);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Start";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(60, 40);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(479, 40);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnIDN
+            // 
+            this.btnIDN.Location = new System.Drawing.Point(372, 40);
+            this.btnIDN.Name = "btnIDN";
+            this.btnIDN.Size = new System.Drawing.Size(75, 23);
+            this.btnIDN.TabIndex = 3;
+            this.btnIDN.Text = "IDN";
+            this.btnIDN.UseVisualStyleBackColor = true;
+            this.btnIDN.Click += new System.EventHandler(this.btnIDN_Click);
+            // 
+            // btnGetBuff
+            // 
+            this.btnGetBuff.Location = new System.Drawing.Point(248, 97);
+            this.btnGetBuff.Name = "btnGetBuff";
+            this.btnGetBuff.Size = new System.Drawing.Size(75, 23);
+            this.btnGetBuff.TabIndex = 4;
+            this.btnGetBuff.Text = "Get Buffer";
+            this.btnGetBuff.UseVisualStyleBackColor = true;
+            this.btnGetBuff.Click += new System.EventHandler(this.btnGetBuff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetBuff);
+            this.Controls.Add(this.btnIDN);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnIDN;
+        private System.Windows.Forms.Button btnGetBuff;
+    }
+}
+
---
+++ DisplayController/DisplayController/SerialDevice.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayController
+{
+    class SerialDevice
+    {
+        SerialPort port;
+
+        public SerialDevice()
+        {
+            port = null; ;
+        }
+
+        ~SerialDevice()
+        {
+            this.Disconnect();
+        }
+
+        static public string[] GetComPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+
+        public bool Connect(string portName)
+        {
+            bool success = false;
+            if (!IsConnected())
+            {
+                port = new SerialPort(portName);
+                if (!port.IsOpen)
+                {
+                    port.BaudRate = 115200;
+                    port.DataBits = 8;
+                    port.StopBits = StopBits.One;
+                    port.Parity = Parity.None;
+                    port.Handshake = Handshake.None;
+                    port.DtrEnable = true;
+                    port.ReadTimeout = 5000;
+                    port.WriteTimeout = 5000;
+                    port.Open();
+
+                    success = this.IsConnected();
+                }
+            }
+            return success;
+        }
+
+        public void Disconnect()
+        {
+            if (this.IsConnected())
+                port.Close();
+        }
+
+        public bool IsConnected()
+        {
+            if (port != null && port.IsOpen)
+                return true;
+            return false;
+        }
+
+
+        public string ReadLine()
+        {
+            String str = "";
+
+            if (IsConnected())
+            {
+                try
+                {
+                    str = port.ReadLine();
+                    str = str.TrimEnd('\r', '\n');
+                }
+                catch   // timeout
+                {
+                    str = "Error";
+                }
+            }
+
+            Console.WriteLine("Arduino: " + str);
+            return str;
+        }
+
+        public void WriteLine(string str)
+        {
+            str = str + "\r";   // add a CR
+            if (IsConnected())
+            {
+                try
+                {
+                    port.DiscardOutBuffer();
+                    port.DiscardInBuffer();
+                    Console.WriteLine("PC: " + str);
+                    port.WriteLine(str);
+                }
+                catch   // timeout
+                {
+                    // write error/timeout
+                    Console.WriteLine("PC: [Write Timout]");
+                }
+            }
+        }
+
+        public string SendRequestCommand(string cmd)
+        {
+            this.WriteLine(cmd);
+            return this.ReadLine();
+        }
+
+        public void SendExecuteCommand(string cmd)
+        {
+            this.WriteLine(cmd);
+        }
+
+        public void SendBytes(byte[] byteArray)
+        {
+            if (IsConnected())
+            {
+                try
+                {
+                    port.Write(byteArray, 0, byteArray.Length);
+                }
+                catch   // timeout
+                {
+                    Console.WriteLine("failed");
+                }
+            }
+        }
+    }
+}
